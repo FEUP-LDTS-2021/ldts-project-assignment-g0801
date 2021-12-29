8693f49987ced2a3d5ca46a81fbc9b43a5cefe5f
Zé Diogo <zediogox@hotmail.com>
2021-12-29 17:23:38 +0000

    Started Implementing GUI

--- src/main/java/TestScreen.java
+++ src/main/java/TestScreen.java
@@ -1,7 +1,17 @@
+import GUI.*;
+import com.g10.supaplex.Size;
+
+import java.io.IOException;
+
 public class TestScreen {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    private final GUI gui;
 
+    public TestScreen() throws IOException {
+        this.gui = new LanternaGUI(new Size(150,50));
+    }
+
+    public static void main(String[] args) throws IOException {
+        TestScreen test = new TestScreen();
     }
 }
