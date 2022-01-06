5566b5ab00668676f29ac79897ef1e802a0b55de
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-06 01:53:42 +0000

    Menu Controller Tests

--- src/test/groovy/com/g801/supaplex/Controller Tests/MenuControllerTest.groovy
+++ src/test/groovy/com/g801/supaplex/Controller Tests/MenuControllerTest.groovy
@@ -13,17 +13,45 @@ class MenuControllerTest extends Specification {
     private time = 900000;
 
     def setup() {
+        game = Mock(Game.class);
         mainMenu = Mock(MainMenu.class);
         menuController = new MenuController(mainMenu);
-        game = Mock(Game.class);
     }
 
-    def "init"() {
+    def "Main menu receives DOWN key pressed"() {
 
         when:
             menuController.execute(game, GUI.KEYACTION.DOWN, time);
         then:
             1 * mainMenu.downButton();
+    }
 
+    def "Main menu receives UP key pressed"() {
+
+        when:
+            menuController.execute(game, GUI.KEYACTION.UP, time);
+        then:
+            1 * mainMenu.upButton();
+    }
+
+    def "Let's begin the game"() {
+
+        given:
+            mainMenu.getCurrentSelect() >> MainMenu.Option.START;
+        when:
+            menuController.execute(game, GUI.KEYACTION.SELECT, time);
+        then:
+            1 * game.run();
+    }
+
+    def "Let's select a level"() {
+
+        given:
+            mainMenu.getCurrentSelect() >> MainMenu.Option.LEVEL;
+        when:
+            menuController.execute(game, GUI.KEYACTION.SELECT, time);
+        then:
+            1 * game.pushState(_);
+            1 * game.getConfiguration();
     }
 }
