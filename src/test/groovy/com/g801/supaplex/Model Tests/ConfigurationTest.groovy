3d3de017bbc933130e78caf2e1af8cef28f26c86
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-12 00:46:33 +0000

    Added configuration and screen tests

--- src/test/groovy/com/g801/supaplex/Model Tests/ConfigurationTest.groovy
+++ src/test/groovy/com/g801/supaplex/Model Tests/ConfigurationTest.groovy
@@ -3,40 +3,105 @@ import com.g801.supaplex.Model.Elements.Murphy
 import com.g801.supaplex.Model.Position
 import spock.lang.Specification;
 import com.g801.supaplex.Model.Configuration;
-import com.g801.supaplex.Model.ScreenSettings;
 
 class ConfigurationTest extends Specification {
 
     private Configuration configuration;
+    private Murphy murphy;
+    private Position bounds;
 
     def setup() {
         configuration = Configuration.getInstance();
+        murphy = Mock(Murphy.class);
+        bounds = new Position(24, 15);
     }
 
     def "Initial configurations"() {
 
+        when:
+            configuration.setMapBounds(24, 15);
+
         expect:
             configuration.getCurrentLevel() == 1;
             configuration.getScreenLowerBound() != null;
-            configuration.getScreenSettings() != null;
-            configuration.getScreenSettings().x == ScreenSettings.x;
-            configuration.getScreenSettings().y == ScreenSettings.y;
+            configuration.getMurphySides() == 5;
+            configuration.getMapBounds() == 3;
     }
 
     def "Updating settings"() {
 
-        given:
-            Murphy mockedMurphy = Mock(Murphy.class);
-            mockedMurphy.getPos() >> new Position(10, 20);
-            Position mockedPosition = Mock(Position.class);
-
         when:
-            configuration.updateSettings(mockedMurphy, mockedPosition);
+            configuration.updateSettings(murphy, new Position(10, 20));
 
         then:
-            2 * mockedMurphy.getPos();
-            1 * mockedPosition.getY();
-            1 * mockedPosition.getX();
+            2 * murphy.getPos();
+            1 * murphy.getPos().getY();
+            1 * murphy.getPos().getX();
+    }
+
+    def "Murphy is in the middle of the map"() {
+
+        given:
+            murphy.getPos() >> new Position(11, 7);
+
+        when:
+            configuration.updateSettings(murphy, bounds);
+
+        then:
+            configuration.getXmin() == 6;
+            configuration.getYmin() == 4;
+    }
+
+    def "Murphy is in the upper left corner"() {
+
+        given:
+            murphy.getPos() >> new Position(3, 2);
+
+        when:
+            configuration.updateSettings(murphy, bounds);
+
+        then:
+            configuration.getXmin() == 0;
+            configuration.getYmin() == 0;
+    }
+
+    def "Murphy is in the upper right corner"() {
+
+        given:
+            murphy.getPos() >> new Position(22, 2);
+
+        when:
+            configuration.updateSettings(murphy, bounds);
+
+        then:
+            configuration.getXmin() == 14;
+            configuration.getYmin() == 0;
+    }
+
+    def "Murphy is in the lower left corner"() {
+
+        given:
+            murphy.getPos() >> new Position(4, 12);
+
+        when:
+            configuration.updateSettings(murphy, bounds);
+
+        then:
+            configuration.getXmin() == 0;
+            configuration.getYmin() == 9;
+    }
+
+    def "Murphy is in the lower right corner"() {
+
+        given:
+            murphy.getPos() >> new Position(24, 14);
+
+        when:
+            configuration.updateSettings(murphy, bounds);
+
+        then:
+            configuration.getXmin() == 14;
+            configuration.getYmin() == 9;
     }
 
     def "Increasing current level"() {
