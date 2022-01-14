fa4a44846ea066c8f189a2ca76d30ca53026cc9d
Zé Diogo <zediogox@hotmail.com>
2022-01-14 01:10:06 +0000

    Implemented Infotron Counter and it's Display

--- src/main/java/com/g801/supaplex/Viewer/Menu/SelectMenuViewer.java
+++ src/main/java/com/g801/supaplex/Viewer/Menu/SelectMenuViewer.java
@@ -1,20 +1,15 @@
 package com.g801.supaplex.Viewer.Menu;
 
 
-import com.g801.supaplex.Model.Menu.Elements.Image;
 import com.g801.supaplex.Model.Menu.SelectLevelMenu;
-import com.g801.supaplex.Model.Position;
 import com.g801.supaplex.Model.Size;
 import com.g801.supaplex.Viewer.GUI.GUI;
 import com.g801.supaplex.Viewer.Viewer;
 import com.googlecode.lanterna.*;
-import com.googlecode.lanterna.graphics.BasicTextImage;
 import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.graphics.TextImage;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
-import java.util.List;
 
 public class SelectMenuViewer extends Viewer <SelectLevelMenu> {
 
@@ -38,12 +33,12 @@ public class SelectMenuViewer extends Viewer <SelectLevelMenu> {
 
         for (SelectLevelMenu.Option elem : getModel().getOpt()) {
             if (getModel().getCurrentSelect() == elem) {
-                gui.drawString(TextColor.ANSI.BLUE_BRIGHT, y, getModel().enumToString(elem));
+                gui.drawStringCentered(TextColor.ANSI.BLUE_BRIGHT, y, getModel().enumToString(elem));
             }
-            else gui.drawString(TextColor.ANSI.RED_BRIGHT, y, getModel().enumToString(elem));
+            else gui.drawStringCentered(TextColor.ANSI.RED_BRIGHT, y, getModel().enumToString(elem));
             y += 2;
         }
-        gui.drawString(TextColor.ANSI.RED_BRIGHT, y, "CURRENT LVL: " + getModel().getConfiguration().getCurrentLevel());
+        gui.drawStringCentered(TextColor.ANSI.RED_BRIGHT, y, "CURRENT LVL: " + getModel().getConfiguration().getCurrentLevel());
 
         gui.drawImages(getModel().getTextImagesList());
     }
