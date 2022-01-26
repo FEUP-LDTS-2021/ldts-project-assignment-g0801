274e536f2542e7041205765011bd9db069d98c0b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-26 20:21:57 +0000

    Mutation Testing, change in organization of test folders

---
+++ src/test/groovy/com/g801/supaplex/Model/Models/ModelTest.groovy
@@ -0,0 +1,32 @@
+package com.g801.supaplex.Model.Models
+
+import com.g801.supaplex.Model.Models.Sprite
+import com.g801.supaplex.Model.Position;
+import com.g801.supaplex.Model.Models.Model
+import spock.lang.Specification
+
+class ModelTest extends Specification {
+
+    private Position position;
+    private Model model;
+
+    def setup() {
+        model = new Model();
+        position = new Position(10, 20);
+    }
+
+    def "Initial configuration"() {
+
+        expect:
+            model.getModel() == null;
+            model.getPos() == null;
+    }
+
+    def "New Position"() {
+
+        given:
+            model.setPos(position);
+        expect:
+            model.getPos() == new Position(10, 20);
+    }
+}
